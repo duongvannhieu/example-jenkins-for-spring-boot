1baa41a7cbfbe4362a128287fa6c9e7e426f4a81
duongvannhieu <duongnhieu5@gmail.com>
2023-10-21 09:14:51 +0700

    init

---
+++ src/main/java/com/examplejenkins/examplejenkins/ExampleJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.examplejenkins.examplejenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examplejenkins/examplejenkins/ExampleJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplejenkins.examplejenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
